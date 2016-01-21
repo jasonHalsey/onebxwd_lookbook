ea3b4eff383b71ba1a3c74bd3bf8d72472fd9e94
Jason Halsey <jasonhalsey@Halsey-C2QNF00HFTPR.local>
2016-01-21 14:09:44 -0800

    replace index.html with holding page

--- index.php
+++ index.php
@@ -1,197 +1,39 @@
-<?php 
-    include("inc/header.php"); 
-?>
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>One Bxwd</title>
+        <meta name="description" content="ONE BXWD’s main purpose is to bring an innovative and comfortable wardrobe to the boundless cross generational male. Using a refined approach by combining performance fabrics with a polished sportswear mind set.">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="stylesheets/screen.css">
 
-        <main class="cd-container">
-            <div class="main">
+        <script src="//use.typekit.net/vvq3khi.js"></script>
+        <script>try{Typekit.load({ async: true });}catch(e){}</script>
+        
 
-                <ul class="image_container">
-                    <li>
+        <script async src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
 
-                        <img
-                            alt=""
-                            src="images/lookbook_2016/05-sm.jpg"
-                            sizes="(min-width: 1000px) 930px, 90vw"
-                            data-srcset="images/lookbook_2016/05-sm.jpg 500w,
-                                images/lookbook_2016/05.jpg 1024w"
-                            class="lazyload" />
-                    </li>
-                    <li>
 
-                        <img
-                            alt=""
-                            src="images/lookbook_2016/01-sm.jpg"
-                            sizes="(min-width: 1000px) 930px, 90vw"
-                            data-srcset="images/lookbook_2016/01-sm.jpg 500w,
-                                images/lookbook_2016/01.jpg 1024w"
-                            class="lazyload" />
-                    </li>
-                    <li>
+         
+    </head>
+    <body class="holding">
 
-                        <img
-                            alt=""
-                            src="images/lookbook_2016/02-sm.jpg"
-                            sizes="(min-width: 1000px) 930px, 90vw"
-                            data-srcset="images/lookbook_2016/02-sm.jpg 500w,
-                                images/lookbook_2016/02.jpg 1024w"
-                            class="lazyload" />
-                    </li>
-                    <li>
+        <div class="main_logo_holding">
+                <img src="images/onebxwd_brand_dkgray.svg" />
+        </div>
 
-                        <img
-                            alt=""
-                            src="images/lookbook_2016/03-sm.jpg"
-                            sizes="(min-width: 1000px) 930px, 90vw"
-                            data-srcset="images/lookbook_2016/03-sm.jpg 500w,
-                                images/lookbook_2016/03.jpg 1024w"
-                            class="lazyload" />
-                    </li>
-                    <li>
+        <footer>
+            <div class="holding_number">
+                310-277-4160
+            </div>
+        </footer>
+        <script  src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script async src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
 
-                        <img
-                            alt=""
-                            src="images/lookbook_2016/03b-sm.jpg"
-                            sizes="(min-width: 1000px) 930px, 90vw"
-                            data-srcset="images/lookbook_2016/03b-sm.jpg 500w,
-                                images/lookbook_2016/03b.jpg 1024w"
-                            class="lazyload" />
-                    </li>
-
-                    <li>
-
-                        <img
-                            alt=""
-                            src="images/lookbook_2016/04-sm.jpg"
-                            sizes="(min-width: 1000px) 930px, 90vw"
-                            data-srcset="images/lookbook_2016/04-sm.jpg 500w,
-                                images/lookbook_2016/04.jpg 1024w"
-                            class="lazyload" />
-                        <div class="byebye_overlay"> This is the overlay</div>
-                    </li>
-                    <li>
-
-                        <img
-                            alt=""
-                            src="images/lookbook_2016/06-sm.jpg"
-                            sizes="(min-width: 1000px) 930px, 90vw"
-                            data-srcset="images/lookbook_2016/06-sm.jpg 500w,
-                                images/lookbook_2016/06.jpg 1024w"
-                            class="lazyload" />
-                    </li>
-                    <li>
-
-                        <img
-                            alt=""
-                            src="images/lookbook_2016/07-sm.jpg"
-                            sizes="(min-width: 1000px) 930px, 90vw"
-                            data-srcset="images/lookbook_2016/07-sm.jpg 500w,
-                                images/lookbook_2016/07.jpg 1024w"
-                            class="lazyload" />
-                    </li>
-                    <li>
-
-                        <img
-                            alt=""
-                            src="images/lookbook_2016/08-sm.jpg"
-                            sizes="(min-width: 1000px) 930px, 90vw"
-                            data-srcset="images/lookbook_2016/08-sm.jpg 500w,
-                                images/lookbook_2016/08.jpg 1024w"
-                            class="lazyload" />
-                    </li>
-                    <li>
-
-                        <img
-                            alt=""
-                            src="images/lookbook_2016/09-sm.jpg"
-                            sizes="(min-width: 1000px) 930px, 90vw"
-                            data-srcset="images/lookbook_2016/09-sm.jpg 500w,
-                                images/lookbook_2016/09.jpg 1024w"
-                            class="lazyload" />
-                    </li>
-                    <li>
-
-                        <img
-                            alt=""
-                            src="images/lookbook_2016/10-sm.jpg"
-                            sizes="(min-width: 1000px) 930px, 90vw"
-                            data-srcset="images/lookbook_2016/10-sm.jpg 500w,
-                                images/lookbook_2016/10.jpg 1024w"
-                            class="lazyload" />
-                    </li>
-                    <li>
-
-                        <img
-                            alt=""
-                            src="images/lookbook_2016/11-sm.jpg"
-                            sizes="(min-width: 1000px) 930px, 90vw"
-                            data-srcset="images/lookbook_2016/11-sm.jpg 500w,
-                                images/lookbook_2016/11.jpg 1024w"
-                            class="lazyload" />
-                    </li>
-                    
-                    <li>
-
-                        <img
-                            alt=""
-                            src="images/lookbook_2016/12-sm.jpg"
-                            sizes="(min-width: 1000px) 930px, 90vw"
-                            data-srcset="images/lookbook_2016/12-sm.jpg 500w,
-                                images/lookbook_2016/12.jpg 1024w"
-                            class="lazyload" />
-                    </li>
-                    
-                    <li>
-
-                        <img
-                            alt=""
-                            src="images/lookbook_2016/13-sm.jpg"
-                            sizes="(min-width: 1000px) 930px, 90vw"
-                            data-srcset="images/lookbook_2016/13-sm.jpg 500w,
-                                images/lookbook_2016/13.jpg 1024w"
-                            class="lazyload" />
-                    </li>
-                    <li>
-
-                        <img
-                            alt=""
-                            src="images/lookbook_2016/14-sm.jpg"
-                            sizes="(min-width: 1000px) 930px, 90vw"
-                            data-srcset="images/lookbook_2016/14-sm.jpg 500w,
-                                images/lookbook_2016/14.jpg 1024w"
-                            class="lazyload" />
-                    </li>
-                    <li>
-
-                        <img
-                            alt=""
-                            src="images/lookbook_2016/15-sm.jpg"
-                            sizes="(min-width: 1000px) 930px, 90vw"
-                            data-srcset="images/lookbook_2016/15-sm.jpg 500w,
-                                images/lookbook_2016/15.jpg 1024w"
-                            class="lazyload" />
-                    </li>
-                    <li>
-
-                        <img
-                            alt=""
-                            src="images/lookbook_2016/16-sm.jpg"
-                            sizes="(min-width: 1000px) 930px, 90vw"
-                            data-srcset="images/lookbook_2016/16-sm.jpg 500w,
-                                images/lookbook_2016/16.jpg 1024w"
-                            class="lazyload" />
-                    </li>
-                    
-                </ul>
-                <section id="instagram" class="content-stripe instafeed-container texture">
-                  <div class="insta-content content-block">
-                    <ul class="jhalzfeed">
-
-                    </ul>
-                  </div>
-                </section>
-            </div> <!-- #main -->
-            <a href="#0" class="cd-top icon-circle-up"></a>
-        </main> <!-- #main-container -->
-
-       
-<?php include_once("inc/footer.php"); ?>
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
---
+++ index_full.php
@@ -0,0 +1,197 @@
+<?php 
+    include("inc/header.php"); 
+?>
+
+        <main class="cd-container">
+            <div class="main">
+
+                <ul class="image_container">
+                    <li>
+
+                        <img
+                            alt=""
+                            src="images/lookbook_2016/05-sm.jpg"
+                            sizes="(min-width: 1000px) 930px, 90vw"
+                            data-srcset="images/lookbook_2016/05-sm.jpg 500w,
+                                images/lookbook_2016/05.jpg 1024w"
+                            class="lazyload" />
+                    </li>
+ <!--                    <li>
+
+                        <img
+                            alt=""
+                            src="images/lookbook_2016/01-sm.jpg"
+                            sizes="(min-width: 1000px) 930px, 90vw"
+                            data-srcset="images/lookbook_2016/01-sm.jpg 500w,
+                                images/lookbook_2016/01.jpg 1024w"
+                            class="lazyload" />
+                    </li> -->
+                    <li>
+
+                        <img
+                            alt=""
+                            src="images/lookbook_2016/02-sm.jpg"
+                            sizes="(min-width: 1000px) 930px, 90vw"
+                            data-srcset="images/lookbook_2016/02-sm.jpg 500w,
+                                images/lookbook_2016/02.jpg 1024w"
+                            class="lazyload" />
+                    </li>
+                    <li>
+
+                        <img
+                            alt=""
+                            src="images/lookbook_2016/03-sm.jpg"
+                            sizes="(min-width: 1000px) 930px, 90vw"
+                            data-srcset="images/lookbook_2016/03-sm.jpg 500w,
+                                images/lookbook_2016/03.jpg 1024w"
+                            class="lazyload" />
+                    </li>
+                    <li>
+
+                        <img
+                            alt=""
+                            src="images/lookbook_2016/03b-sm.jpg"
+                            sizes="(min-width: 1000px) 930px, 90vw"
+                            data-srcset="images/lookbook_2016/03b-sm.jpg 500w,
+                                images/lookbook_2016/03b.jpg 1024w"
+                            class="lazyload" />
+                    </li>
+
+                    <li>
+
+                        <img
+                            alt=""
+                            src="images/lookbook_2016/04-sm.jpg"
+                            sizes="(min-width: 1000px) 930px, 90vw"
+                            data-srcset="images/lookbook_2016/04-sm.jpg 500w,
+                                images/lookbook_2016/04.jpg 1024w"
+                            class="lazyload" />
+                        <div class="byebye_overlay"> This is the overlay</div>
+                    </li>
+                    <li>
+
+                        <img
+                            alt=""
+                            src="images/lookbook_2016/06-sm.jpg"
+                            sizes="(min-width: 1000px) 930px, 90vw"
+                            data-srcset="images/lookbook_2016/06-sm.jpg 500w,
+                                images/lookbook_2016/06.jpg 1024w"
+                            class="lazyload" />
+                    </li>
+                    <li>
+
+                        <img
+                            alt=""
+                            src="images/lookbook_2016/07-sm.jpg"
+                            sizes="(min-width: 1000px) 930px, 90vw"
+                            data-srcset="images/lookbook_2016/07-sm.jpg 500w,
+                                images/lookbook_2016/07.jpg 1024w"
+                            class="lazyload" />
+                    </li>
+                    <li>
+
+                        <img
+                            alt=""
+                            src="images/lookbook_2016/08-sm.jpg"
+                            sizes="(min-width: 1000px) 930px, 90vw"
+                            data-srcset="images/lookbook_2016/08-sm.jpg 500w,
+                                images/lookbook_2016/08.jpg 1024w"
+                            class="lazyload" />
+                    </li>
+                    <li>
+
+                        <img
+                            alt=""
+                            src="images/lookbook_2016/09-sm.jpg"
+                            sizes="(min-width: 1000px) 930px, 90vw"
+                            data-srcset="images/lookbook_2016/09-sm.jpg 500w,
+                                images/lookbook_2016/09.jpg 1024w"
+                            class="lazyload" />
+                    </li>
+                    <li>
+
+                        <img
+                            alt=""
+                            src="images/lookbook_2016/10-sm.jpg"
+                            sizes="(min-width: 1000px) 930px, 90vw"
+                            data-srcset="images/lookbook_2016/10-sm.jpg 500w,
+                                images/lookbook_2016/10.jpg 1024w"
+                            class="lazyload" />
+                    </li>
+                    <li>
+
+                        <img
+                            alt=""
+                            src="images/lookbook_2016/11-sm.jpg"
+                            sizes="(min-width: 1000px) 930px, 90vw"
+                            data-srcset="images/lookbook_2016/11-sm.jpg 500w,
+                                images/lookbook_2016/11.jpg 1024w"
+                            class="lazyload" />
+                    </li>
+                    
+                    <li>
+
+                        <img
+                            alt=""
+                            src="images/lookbook_2016/12-sm.jpg"
+                            sizes="(min-width: 1000px) 930px, 90vw"
+                            data-srcset="images/lookbook_2016/12-sm.jpg 500w,
+                                images/lookbook_2016/12.jpg 1024w"
+                            class="lazyload" />
+                    </li>
+                    
+                    <li>
+
+                        <img
+                            alt=""
+                            src="images/lookbook_2016/13-sm.jpg"
+                            sizes="(min-width: 1000px) 930px, 90vw"
+                            data-srcset="images/lookbook_2016/13-sm.jpg 500w,
+                                images/lookbook_2016/13.jpg 1024w"
+                            class="lazyload" />
+                    </li>
+                    <li>
+
+                        <img
+                            alt=""
+                            src="images/lookbook_2016/14-sm.jpg"
+                            sizes="(min-width: 1000px) 930px, 90vw"
+                            data-srcset="images/lookbook_2016/14-sm.jpg 500w,
+                                images/lookbook_2016/14.jpg 1024w"
+                            class="lazyload" />
+                    </li>
+                    <li>
+
+                        <img
+                            alt=""
+                            src="images/lookbook_2016/15-sm.jpg"
+                            sizes="(min-width: 1000px) 930px, 90vw"
+                            data-srcset="images/lookbook_2016/15-sm.jpg 500w,
+                                images/lookbook_2016/15.jpg 1024w"
+                            class="lazyload" />
+                    </li>
+                    <li>
+
+                        <img
+                            alt=""
+                            src="images/lookbook_2016/16-sm.jpg"
+                            sizes="(min-width: 1000px) 930px, 90vw"
+                            data-srcset="images/lookbook_2016/16-sm.jpg 500w,
+                                images/lookbook_2016/16.jpg 1024w"
+                            class="lazyload" />
+                    </li>
+                    
+                </ul>
+                <section id="instagram" class="content-stripe instafeed-container texture">
+                  <div class="insta-content content-block">
+                    <ul class="jhalzfeed">
+
+                    </ul>
+                  </div>
+                </section>
+            </div> <!-- #main -->
+            <a href="#0" class="cd-top icon-circle-up"></a>
+        </main> <!-- #main-container -->
+
+       
+<?php include_once("inc/footer.php"); ?>
